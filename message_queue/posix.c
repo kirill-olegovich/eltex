9953f89571c96fe4c1d8cf092fb8c2b09e42fea7
Kirill Olegovich <verbun236@gmail.com>
2022-06-09 01:04:30 +0700

    123

---
+++ message_queue/posix.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <mqueue.h>
+
+
+int main(void) {
+	pid_t pid;
+	pid = fork();
+
+	if (pid == 0) {
+		struct mq_attr attr;
+		mqd_t mqid;
+		
+		memset(&attr, 0, sizeof attr);
+		attr.mq_msgsize = 4096;
+		attr.mq_maxmsg = 10;
+
+		mqid = mq_open("/buffer", O_CREAT | O_WRONLY, 0600, &attr);
+
+		if (mqid == -1) {
+			perror("mq_open");
+			exit(1);
+		}
+
+		mq_send(mqid, "qweasdzxc", 9, 0);
+		mq_close(mqid);
+
+		exit(0);
+
+	} else if (pid > 0) {
+		int child_status;
+		waitpid(pid, &child_status, 0);
+
+		if (WEXITSTATUS(child_status) == 1) exit(1);
+
+		struct mq_attr attr;
+		unsigned int prior = 0;
+		mqd_t mqid;
+
+		mqid = mq_open("/buffer", O_RDONLY);
+
+		if (mqid == -1) {
+			perror("mq_open");
+			exit(1);
+		}
+
+		mq_getattr(mqid, &attr);
+		char buffer[attr.mq_msgsize];
+
+		if ((mq_receive(mqid, buffer, attr.mq_msgsize, &prior)) == -1) {
+			perror("mq_receive");
+			exit(1);
+		}
+
+		printf("%s %ld\n", buffer, attr.mq_msgsize);
+
+		mq_close(mqid);
+		mq_unlink("/buffer");
+
+	} else printf("Error\n");
+
+	return 0;
+}
